06247bbc62feb8a81f8c1ccdc35cacca12ae4c77
JacobHigbie <147549763+JacobHigbie@users.noreply.github.com>
2023-10-28 17:13:49 -0500

    bingo board generation
    Bingo board is set up. right now, only the top left tile is randomized.

--- Assets/generateTiles.cs
+++ Assets/generateTiles.cs
@@ -14,15 +14,18 @@ public class tiles{
 
 public class generateTiles : MonoBehaviour
 {
+    SpriteRenderer spriteRenderer;
     public Button[] PlBtns;
     public GameObject[] BingoTxt;
-    public Sprite[] sprites;
+    public Sprite[] tileSprites;
+    //public Image[] images;
     public int[] MarkedSpace;
     public List<int> Numbers = new List<int>();
     public GameManager gameManager;
     public static int difficulty;
     public static int environment;
     public static int size;
+    public Image test;
 
     // Start is called before the first frame update
     private void Start(){
@@ -35,8 +38,8 @@ public class generateTiles : MonoBehaviour
         Debug.Log(environment);
         Debug.Log(size);
 
-        tiles[] tilesArr = new tiles[27];
-        for(int i = 0; i<27; i++) {
+        tiles[] tilesArr = new tiles[31];
+        for(int i = 0; i<=30; i++) {
             tiles tempTile = new tiles();
             tilesArr[i] = tempTile;
             tilesArr[i].isChecked = false;
@@ -44,56 +47,60 @@ public class generateTiles : MonoBehaviour
             tilesArr[i].posY = -1;
         }
 
-        //name each tile + tie them to corresponding png
-        tilesArr[0].objName = "Blue Vehicle";
-        tilesArr[1].objName = "Boat";
-        tilesArr[2].objName = "Caution Stripes";
-        tilesArr[3].objName = "Chain Link Fence";
-        tilesArr[4].objName = "Dirt Road";
-        tilesArr[5].objName = "Dog";
-        tilesArr[6].objName = "Fallen Log";
-        tilesArr[7].objName = "Fire Escape";
-        tilesArr[8].objName = "Firetruck";
-        tilesArr[9].objName = "Graffiti";
-        tilesArr[10].objName = "Green Vehicle";
-        tilesArr[11].objName = "Hard Hat";
-        tilesArr[12].objName = "Jeep";
-        tilesArr[13].objName = "Ladder";
-        tilesArr[14].objName = "Litter";
-        tilesArr[15].objName = "No Parking Zone";
-        tilesArr[16].objName = "Pickett Fence";
-        tilesArr[17].objName = "Pickup Truck";
-        tilesArr[18].objName = "Ranch Fence";
-        tilesArr[19].objName = "Recycling Logo";
-        tilesArr[20].objName = "Red Vehicle";
-        tilesArr[21].objName = "Roadkill";
-        tilesArr[22].objName = "Railroad Tracks";
-        tilesArr[23].objName = "Building With 3+ Floors";
-        tilesArr[24].objName = "Trailer";
-        tilesArr[25].objName = "Wildflowers";
-        tilesArr[26].objName = "Yellow Vehicle";
+        //name each tile
+        tilesArr[0].objName = "Bench";
+        tilesArr[1].objName = "Bird of Prey";
+        tilesArr[2].objName = "Blue Vehicle";
+        tilesArr[3].objName = "Boat";
+        tilesArr[4].objName = "Bridge";
+        tilesArr[5].objName = "Caution Stripes";
+        tilesArr[6].objName = "Chain Link Fence";
+        tilesArr[7].objName = "Deer";
+        tilesArr[8].objName = "Dirt Road";
+        tilesArr[9].objName = "Dog";
+        tilesArr[10].objName = "Fallen Log";
+        tilesArr[11].objName = "Fire Escape";
+        tilesArr[12].objName = "Firetruck";
+        tilesArr[13].objName = "Graffiti";
+        tilesArr[14].objName = "Green Vehicle";
+        tilesArr[15].objName = "Hard Hat";
+        tilesArr[16].objName = "Jeep";
+        tilesArr[17].objName = "Ladder";
+        tilesArr[18].objName = "Litter";
+        tilesArr[19].objName = "No Parking Sign";
+        tilesArr[20].objName = "Pickett Fence";
+        tilesArr[21].objName = "Pickup Truck";
+        tilesArr[22].objName = "Ranch Fence";
+        tilesArr[23].objName = "Recyling Logo";
+        tilesArr[24].objName = "Red Vehicle";
+        tilesArr[25].objName = "Roadkill";
+        tilesArr[26].objName = "Railroad Tracks";
+        tilesArr[27].objName = "Building With 3+ Floors";
+        tilesArr[28].objName = "Trailer";
+        tilesArr[29].objName = "Wildflowers";
+        tilesArr[30].objName = "Yellow Vehicle";
     }
 
     void SetupBoard(){
-        //put 1-27 on a list
-        for (int i = 0; i < 27; i++)
+        //put 0-30 on a list
+        for (int i = 0; i <= 30; i++)
         {
-            Numbers.Add(i + 1);
+            Numbers.Add(i);
         }
 
         //shuffle list
         for (int i = 0; i < Numbers.Count; i++)
         {
-            int RandomNum = Random.Range(1, 27);
+            int RandomNum = Random.Range(0, 30);
             int temp = Numbers[RandomNum];
             Numbers[RandomNum] = Numbers[i];
             Numbers[i] = temp;
         }
 
         //put images on corresponding buttons based on list
-        for (int i = 0; i < sprites.Length; i++)
+        for (int i = 0; i < tileSprites.Length; i++)
         {
-            //TxtBox[i].text = Numbers[i].ToString();
+            test.sprite = tileSprites[Numbers[i]];
         }
 
         gameManager = FindObjectOfType<GameManager>();
